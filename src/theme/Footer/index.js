aaa037dba6c49289ba76d652c7856c181b0dc1d3
Deokti <reilgelern@mail.ru>
2021-08-29 16:45:45 +0300

    fix: added links to GitHub, Discord, Telegram, Email. Not yet added links to Twitter, Facebook, Medium.

--- src/theme/Footer/index.js
+++ src/theme/Footer/index.js
@@ -21,9 +21,9 @@ import FacebookIcon from "../../components/icons/FacebookIcon";
 import MailIcon from "../../components/icons/MailIcon";
 
 const footerSocials = [
-  { id: 0, component: <GitHubIcon />, href: '', type: 'link' },
-  { id: 1, component: <DiscordIcon />, href: '', type: 'link' },
-  { id: 2, component: <TelegramIcon />, href: '', type: 'link' },
+  { id: 0, component: <GitHubIcon />, href: 'https://github.com/dapplets', type: 'link' },
+  { id: 1, component: <DiscordIcon />, href: 'https://discord.gg/YcxbkcyjMV ', type: 'link' },
+  { id: 2, component: <TelegramIcon />, href: 'https://t.me/dapplets ', type: 'link' },
   { id: 3, component: <MediumIcon />, href: '', type: 'link' },
   { id: 4, component: <TwitterIcon />, href: '', type: 'link' },
   { id: 5, component: <FacebookIcon />, href: '', type: 'link' },
