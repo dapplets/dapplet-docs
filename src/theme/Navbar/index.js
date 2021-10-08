c7551272709922251da8d4046428497589b14e07
Deokti <reilgelern@mail.ru>
2021-10-08 14:53:39 +0300

    feat: added bottom nav bottom menu to navbar

--- src/theme/Navbar/index.js
+++ src/theme/Navbar/index.js
@@ -33,6 +33,21 @@ function splitNavItemsByPosition(items) {
 }
 
 function Navbar() {
+  const [isHover, setIsHover] = useState(false);
+
+  const onMouseOver = (event) => {
+    if (event.target.classList.contains('home-hover-menu')) {
+      setIsHover(true);
+    }
+  }
+
+  const onMouseOut = (event) => {
+    if (event.target.classList.contains('home-hover-menu')) {
+      setIsHover(false);
+    }
+  }
+
+
   const {
     navbar: {
       items,
@@ -72,63 +87,89 @@ function Navbar() {
     leftItems,
     rightItems
   } = splitNavItemsByPosition(items);
-  return <nav ref={navbarRef} className={clsx('navbar', 'navbar--fixed-top', {
-    'navbar--dark': style === 'dark',
-    'navbar--primary': style === 'primary',
-    'navbar-sidebar--show': sidebarShown,
-    [styles.navbarHideable]: hideOnScroll,
-    [styles.navbarHidden]: !isNavbarVisible
-  })}>
-    <div className="navbar__inner">
-      <div className="navbar__items">
-        {items != null && items.length !== 0 && <div aria-label="Navigation bar toggle" className="navbar__toggle" role="button" tabIndex={0} onClick={showSidebar} onKeyDown={showSidebar}>
-          <IconMenu />
-        </div>}
-        <Logo className="navbar__brand" imageClassName="navbar__logo" titleClassName={clsx('navbar__title', {
-          [styles.hideLogoText]: isSearchBarExpanded
-        })} />
-        {leftItems.map((item, i) => <NavbarItem {...item} key={i} />)}
-      </div>
-      <div className="navbar__items navbar__items--right">
-        {rightItems.map((item, i) => <NavbarItem {...item} key={i} />)}
-        {!disableColorModeSwitch && <Toggle className={styles.displayOnlyInLargeViewport} aria-label="Dark mode toggle" checked={isDarkTheme} onChange={onToggleChange} />}
-        <SearchBar handleSearchBarToggle={setIsSearchBarExpanded} isSearchBarExpanded={isSearchBarExpanded} />
-      </div>
-    </div>
-    <div role="presentation" className="navbar-sidebar__backdrop" onClick={hideSidebar} />
-    <div className="navbar-sidebar">
-      <div className="navbar-sidebar__brand">
-        <Logo className="navbar__brand" imageClassName="navbar__logo" titleClassName="navbar__title" onClick={hideSidebar} />
-        {!disableColorModeSwitch && sidebarShown && <Toggle aria-label="Dark mode toggle in sidebar" checked={isDarkTheme} onChange={onToggleChange} />}
-      </div>
-      <div className="navbar-sidebar__items">
-        <div className="menu">
-          <ul className="menu__list">
-            {items.map((item, i) => <NavbarItem mobile {...item} onClick={hideSidebar} key={i} />)}
-          </ul>
+  return (
+    <nav ref={navbarRef} className={clsx('navbar', 'navbar--fixed-top', {
+      'navbar--dark': style === 'dark',
+      'navbar--primary': style === 'primary',
+      'navbar-sidebar--show': sidebarShown,
+      [styles.navbarHideable]: hideOnScroll,
+      [styles.navbarHidden]: !isNavbarVisible,
+      'home-hover-menu-show': isHover,
+    })}>
+      <div className="navbar__inner">
+        <div className="navbar__items">
+          {items != null && items.length !== 0 && <div aria-label="Navigation bar toggle" className="navbar__toggle" role="button" tabIndex={0} onClick={showSidebar} onKeyDown={showSidebar}>
+            <IconMenu />
+          </div>}
+          <Logo className="navbar__brand" imageClassName="navbar__logo" titleClassName={clsx('navbar__title', {
+            [styles.hideLogoText]: isSearchBarExpanded
+          })} />
+          {leftItems.map((item, i) => <NavbarItem {...item} key={i} />)}
+        </div>
+        <div className="navbar__items navbar__items--right">
+          {rightItems.map((item, i) => <NavbarItem {...item} key={i} onMouseOver={onMouseOver} onMouseOut={onMouseOut} />)}
+          {!disableColorModeSwitch && <Toggle className={styles.displayOnlyInLargeViewport} aria-label="Dark mode toggle" checked={isDarkTheme} onChange={onToggleChange} />}
+          <SearchBar handleSearchBarToggle={setIsSearchBarExpanded} isSearchBarExpanded={isSearchBarExpanded} />
         </div>
       </div>
-    </div>
+
+      <div role="presentation" className="navbar-sidebar__backdrop" onClick={hideSidebar} />
+      <div className="navbar-sidebar">
+        <div className="navbar-sidebar__brand">
+          <Logo className="navbar__brand" imageClassName="navbar__logo" titleClassName="navbar__title" onClick={hideSidebar} />
+          {!disableColorModeSwitch && sidebarShown && <Toggle aria-label="Dark mode toggle in sidebar" checked={isDarkTheme} onChange={onToggleChange} />}
+        </div>
+        <div className="navbar-sidebar__items">
+          <div className="menu">
+            <ul className="menu__list">
+              {items.map((item, i) => <NavbarItem mobile {...item} onClick={hideSidebar} key={i} />)}
+            </ul>
+          </div>
+        </div>
+      </div>
 
 
-    <ul className="header-custom-socials">
-      {
-        headerSocials.map(({ id, component, href, title }) => {
-          return (
-            <li key={id} className="header-custom-social" title={title}>
-              <a href={href} target="_blank">
-                {component}
-              </a>
-            </li>
-          )
-        })
-      }
-    </ul>
+      <ul className="header-custom-socials">
+        {
+          headerSocials.map(({ id, component, href, title }) => {
+            return (
+              <li key={id} className="header-custom-social" title={title}>
+                <a href={href} target="_blank">
+                  {component}
+                </a>
+              </li>
+            )
+          })
+        }
+      </ul>
 
-    <button className="header-custom-search">
-      <SearchIcon />
-    </button>
-  </nav>;
+      <button className="header-custom-search">
+        <SearchIcon />
+      </button>
+
+      {/* CUSTOM START */}
+      <div className="header-custom-nav">
+        <ul className="header__nav-list">
+          <li className="header__nav-item">
+            <a href="https://dapplets.org/under-construction.html" className="header__nav-link" target="_blank">Store</a>
+          </li>
+          <li className="header__nav-item">
+            <a href="https://dapplets.org/developers.html" className="header__nav-link" target="_blank">Developers</a>
+          </li>
+          <li className="header__nav-item">
+            <a href="https://dapplets.org/join-us.html" className="header__nav-link" target="_blank">Join Us</a>
+          </li>
+          <li className="header__nav-item">
+            <a href="https://blog.dapplets.org/" className="header__nav-link" target="_blank">Blog</a>
+          </li>
+          <li className="header__nav-item">
+            <a href="https://dapplets.org/about.html" className="header__nav-link" target="_blank">About</a>
+          </li>
+        </ul>
+      </div>
+      {/* CUSTOM END */}
+    </nav>
+  )
 }
 
 export default Navbar;
